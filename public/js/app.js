0c9e683fa0763fb92dd20c9303e03d4094952f81
olivierhsta <olivier@hassaoui.com>
2019-04-19 22:08:39 -0400

    Fix vulnerability

--- public/js/app.js
+++ public/js/app.js
@@ -13140,8 +13140,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\olivi\xampp\htdocs\til\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\olivi\xampp\htdocs\til\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\olivi\xampp\htdocs\olivierhsta\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\olivi\xampp\htdocs\olivierhsta\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
